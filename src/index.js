3e77721501f9eb654f0f0430774505f4a00d490a
girishyadav <girishayadav.com>
2024-06-06 14:04:45 +0530

    other chnages

--- src/index.js
+++ src/index.js
@@ -3,7 +3,5 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
 
-// ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
